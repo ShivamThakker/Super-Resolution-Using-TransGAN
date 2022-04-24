fb1c487253864903ea2bcb380a8d2db494e78f06
ShivamThakker <70203436+ShivamThakker@users.noreply.github.com>
2022-04-24 15:32:24 +0530

    Update train_derived.py

--- Code/TransGAN/train_derived.py
+++ Code/TransGAN/train_derived.py
@@ -7,8 +7,7 @@ import models_search
 import datasets
 from functions import train, validate, save_samples, LinearLrDecay, load_params, copy_params, cur_stages
 from utils.utils import set_log_dir, save_checkpoint, create_logger
-# from utils.inception_score import _init_inception
-# from utils.fid_score import create_inception_graph, check_or_download_inception
+
 
 import torch
 import torch.multiprocessing as mp
@@ -23,16 +22,10 @@ from copy import deepcopy
 from adamw import AdamW
 import random 
 
-# torch.backends.cudnn.enabled = True
-# torch.backends.cudnn.benchmark = True
-
 
 def main():
     args = cfg.parse_args()
     
-#     _init_inception()
-#     inception_path = check_or_download_inception(None)
-#     create_inception_graph(inception_path)
     
     if args.seed is not None:
         torch.manual_seed(args.random_seed)
@@ -54,11 +47,9 @@ def main():
 
     ngpus_per_node = torch.cuda.device_count()
     if args.multiprocessing_distributed:
-        # Since we have ngpus_per_node processes per node, the total world_size
-        # needs to be adjusted accordingly
+
         args.world_size = ngpus_per_node * args.world_size
-        # Use torch.multiprocessing.spawn to launch distributed processes: the
-        # main_worker process function
+
         mp.spawn(main_worker, nprocs=ngpus_per_node, args=(ngpus_per_node, args))
     else:
         # Simply call main_worker function
@@ -74,8 +65,7 @@ def main_worker(gpu, ngpus_per_node, args):
         if args.dist_url == "env://" and args.rank == -1:
             args.rank = int(os.environ["RANK"])
         if args.multiprocessing_distributed:
-            # For multiprocessing distributed training, rank needs to be the
-            # global rank among all the processes
+
             args.rank = args.rank * ngpus_per_node + gpu
         dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
                                 world_size=args.world_size, rank=args.rank)
@@ -91,15 +81,7 @@ def main_worker(gpu, ngpus_per_node, args):
                 nn.init.xavier_uniform(m.weight.data, 1.)
             else:
                 raise NotImplementedError('{} unknown inital type'.format(args.init_type))
-#         elif classname.find('Linear') != -1:
-#             if args.init_type == 'normal':
-#                 nn.init.normal_(m.weight.data, 0.0, 0.02)
-#             elif args.init_type == 'orth':
-#                 nn.init.orthogonal_(m.weight.data)
-#             elif args.init_type == 'xavier_uniform':
-#                 nn.init.xavier_uniform(m.weight.data, 1.)
-#             else:
-#                 raise NotImplementedError('{} unknown inital type'.format(args.init_type))
+
         elif classname.find('BatchNorm2d') != -1:
             nn.init.normal_(m.weight.data, 1.0, 0.02)
             nn.init.constant_(m.bias.data, 0.0)
@@ -110,9 +92,7 @@ def main_worker(gpu, ngpus_per_node, args):
     if not torch.cuda.is_available():
         print('using CPU, this will be slow')
     elif args.distributed:
-        # For multiprocessing distributed, DistributedDataParallel constructor
-        # should always set the single device scope, otherwise,
-        # DistributedDataParallel will use all available devices.
+
         if args.gpu is not None:
             
             torch.cuda.set_device(args.gpu)
@@ -123,9 +103,7 @@ def main_worker(gpu, ngpus_per_node, args):
             dis_net.apply(weights_init)
             gen_net.cuda(args.gpu)
             dis_net.cuda(args.gpu)
-            # When using a single GPU per process and per
-            # DistributedDataParallel, we need to divide the batch size
-            # ourselves based on the total number of GPUs we have
+
             args.dis_batch_size = int(args.dis_batch_size / ngpus_per_node)
             args.gen_batch_size = int(args.gen_batch_size / ngpus_per_node)
             args.batch_size = args.dis_batch_size
@@ -136,8 +114,7 @@ def main_worker(gpu, ngpus_per_node, args):
         else:
             gen_net.cuda()
             dis_net.cuda()
-            # DistributedDataParallel will divide and allocate batch_size to all
-            # available GPUs if device_ids are not set
+
             gen_net = torch.nn.parallel.DistributedDataParallel(gen_net)
             dis_net = torch.nn.parallel.DistributedDataParallel(dis_net)
     elif args.gpu is not None:
@@ -215,10 +192,7 @@ def main_worker(gpu, ngpus_per_node, args):
         gen_avg_param = copy_params(gen_net, mode='gpu')
         gen_net.load_state_dict(checkpoint['gen_state_dict'])
         fixed_z = checkpoint['fixed_z']
-#         del avg_gen_net
-#         gen_avg_param = list(p.cuda().to(f"cuda:{args.gpu}") for p in gen_avg_param)
-        
-        
+
 
         args.path_helper = checkpoint['path_helper']
         logger = create_logger(args.path_helper['log_path']) if args.rank == 0 else None
